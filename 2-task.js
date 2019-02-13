9de2c2aad452e2dba1ee2be62e8d0d49aca26e97
Nikolay Dolgushin <dolgushin111@yandex.ru>
2019-02-14 00:58:03 +0700

    commented on the test

--- 2-task.js
+++ 2-task.js
@@ -7,14 +7,14 @@ module.exports = function (nums, k) {
     }
 }
 
-const nums = [10, 15, 3, 7], k = 17
-
-function sum(nums, k) {
-    for (let i in nums) {
-        s = k - nums[i]
-        n = nums.indexOf(s)
-        Boolean(n !== -1)
-    }
-}
-
-sum(nums, k)
+// const nums = [10, 15, 3, 7], k = 17
+//
+// function sum(nums, k) {
+//     for (let i in nums) {
+//         s = k - nums[i]
+//         n = nums.indexOf(s)
+//         Boolean(n !== -1)
+//     }
+// }
+//
+// sum(nums, k)
--- 3-task.js
+++ 3-task.js
@@ -14,15 +14,15 @@ module.exports = function merge(nums1, m, nums2, n) {
     }
 }
 
-const nums1 = [46, 55, 88, 0, 0, 0, 0], m = 3, nums2 = [18, 29, 80, 90], n = 4
-
-function merge(nums1, m, nums2, n) {
-    for (let i in nums2) {
-        nums1.splice(m, n, nums2[i])
-        m++
-        n--
-    }
-    console.log(nums1)
-}
-
-merge(nums1, m, nums2, n)
+// const nums1 = [46, 55, 88, 0, 0, 0, 0], m = 3, nums2 = [18, 29, 80, 90], n = 4
+//
+// function merge(nums1, m, nums2, n) {
+//     for (let i in nums2) {
+//         nums1.splice(m, n, nums2[i])
+//         m++
+//         n--
+//     }
+//     console.log(nums1)
+// }
+//
+// merge(nums1, m, nums2, n)
